713e13159cafd57168b18ad392e02e1ac89a122d
Nikk1701 <jpmfw01@gmail.com>
2025-06-17 21:23:07 -0300

    Update Conexao.java

--- AcademiaApp/src/main/java/com/mycompany/academiaapp/Conexao.java
+++ AcademiaApp/src/main/java/com/mycompany/academiaapp/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
     private static final String URL = "jdbc:mysql://localhost:3306/academia?useSSL=false&serverTimezone=UTC";
     private static final String USUARIO = "root";
-    private static final String SENHA = ""; // coloque a senha do seu MySQL aqui, se tiver
+    private static final String SENHA = "";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
